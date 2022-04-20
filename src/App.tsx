765d2feca5cf5a27bd7a21717facdf01b99d7de8
Jong jae Park <pjj186@naver.com>
2022-04-20 22:29:19 +0900

    practice

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import styled, { ThemeConsumer } from "styled-components";
+
+interface DummyProps {
+  text: string;
+}
+
+const Container = styled.div`
+  background-color: ${(props) => props.theme.bgColor};
+`;
+const H1 = styled.h1`
+  color: ${(props) => props.theme.textColor};
+`;
+
+function Dummy({ text }: DummyProps) {
+  return <h1>{text}</h1>;
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Dummy text="hello" />
+    </Container>
   );
 }
 
