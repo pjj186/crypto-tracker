fe0e833345d86f070c686980ba4cdad78bbd9df3
Jong jae Park <pjj186@naver.com>
2022-04-25 22:06:23 +0900

    스타일 셋업

--- src/theme.ts
+++ src/theme.ts
@@ -1,7 +1,7 @@
 import { DefaultTheme } from "styled-components";
 
 export const theme: DefaultTheme = {
-  bgColor: "white",
-  textColor: "black",
-  btnColor: "tomato",
+  bgColor: "#2f3640",
+  textColor: "#F5F6FA",
+  accentColor: "#f9ca24",
 };
